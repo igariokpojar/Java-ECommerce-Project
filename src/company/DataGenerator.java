510372c2a53276d1e16dba4d77fabdaebf20ed37
Igariok <igariokpojar@yahoo.com>
2022-08-10 18:39:49 -0700

    Created Static Constants

---
+++ src/company/DataGenerator.java
@@ -0,0 +1,12 @@
+package company;
+
+public class DataGenerator {
+
+    public static void createCustomer(){
+
+        Address address1Customer1 = new Address("7925","Jones Branch Dr","Suite 3300","22102","VA");
+        Address address2Customer1 = new Address("825","GeorgeTown Pky","Suite 5355","22036","VA");
+        Address address1Customer2 = new Address("5924","Lee Hwy","House","22044","VA");
+
+    }
+}
---
+++ src/company/StaticConstants.java
@@ -0,0 +1,9 @@
+package company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaticConstants {
+
+    public static final List<Costumer>COSTUMER_LIST = new ArrayList<>();
+}
