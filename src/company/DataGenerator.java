6ba7f8cec36d1c5ad6c5565c1c90600c10fa9ab9
Igariok <igariokpojar@yahoo.com>
2022-08-10 18:49:19 -0700

    Created costumer method

--- src/company/DataGenerator.java
+++ src/company/DataGenerator.java
@@ -1,5 +1,9 @@
 package company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class DataGenerator {
 
     public static void createCustomer(){
@@ -8,5 +12,16 @@ public class DataGenerator {
         Address address2Customer1 = new Address("825","GeorgeTown Pky","Suite 5355","22036","VA");
         Address address1Customer2 = new Address("5924","Lee Hwy","House","22044","VA");
 
+
+        List<Address>customer1AddressList = new ArrayList<>();
+
+        customer1AddressList.add(address1Customer1);
+        customer1AddressList.add(address2Customer1);
+
+        Costumer customer1 = new Costumer(UUID.randomUUID(),"Igor","igariokpojar@yahoo.com");
+        Costumer costumer2 = new Costumer(UUID.randomUUID(),"Alla", "pojarigariok@gmail.com");
+
+        StaticConstants.COSTUMER_LIST.add(customer1);
+        StaticConstants.COSTUMER_LIST.add(costumer2);
     }
 }
