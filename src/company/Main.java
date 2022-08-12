94539e64c3b9598e768acec8a17f086508a2d4d1
Igariok <igariokpojar@yahoo.com>
2022-08-11 19:15:34 -0700

    Selecting the customer through the scanner

--- src/company/Main.java
+++ src/company/Main.java
@@ -1,5 +1,7 @@
 package company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,5 +14,20 @@ public class Main {
 
        // createBalance();
       //  createDiscount();
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selects Customer");
+
+        for (int i = 0; i < StaticConstants.COSTUMER_LIST.size(); i++) {
+            System.out.println("Type: " + i + " for customer:"+StaticConstants.COSTUMER_LIST.get(i).getUserName());
+
+        }
+        Costumer costumer = StaticConstants.COSTUMER_LIST.get(scanner.nextInt());
+        System.out.println(costumer.getUserName());
+
+
+
     }
 }
