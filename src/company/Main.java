5f8d173b30acdee6a53bc98e7e74cad87448ca8c
Igariok <igariokpojar@yahoo.com>
2022-08-11 19:27:10 -0700

    Switch case created for menu selection

--- src/company/Main.java
+++ src/company/Main.java
@@ -35,6 +35,35 @@ public class Main {
 
             }
 
+            int menuSelection = scanner.nextInt();
+
+            switch (menuSelection){
+                case 0:
+
+
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+            }
+
             prepareMenuOptions();
         }
 
@@ -45,4 +74,6 @@ public class Main {
                 "Place an order","See Cart","See order details","See your address","Close App"};
 
     }
+
+
 }
