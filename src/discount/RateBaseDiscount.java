f58b53b9274bd6f80ffb64d4ea455587d3105b5f
Igariok <igariokpojar@yahoo.com>
2022-08-12 19:10:07 -0700

    Created Discount Abstract class and AmountBaseDiscount, RateBaseDiscount child classes

---
+++ src/discount/RateBaseDiscount.java
@@ -0,0 +1,22 @@
+package discount;
+
+import java.util.UUID;
+
+public class RateBaseDiscount extends Discount{
+
+    private Double rateAmount;
+
+    public RateBaseDiscount(UUID id, String name, Double thresholdAmount, Double rateAmount) {
+        super(id, name, thresholdAmount);
+        this.rateAmount = rateAmount;
+    }
+
+    @Override
+    public Double calculateCartAmountAfterDiscountApplied(Double amount) {
+        return amount-(amount*rateAmount/100);
+    }
+
+    public Double getRateAmount() {
+        return rateAmount;
+    }
+}
